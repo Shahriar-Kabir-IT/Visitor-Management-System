875088087da90d234bd1106bdd4c86805946c91f
Md. Shahriar Kabir (ICT Department) <shahriar.ict@ananta.com>
2025-06-16 14:55:30 +0600

    H.O Done

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+
+<footer class="footer">
+	<div class="d-sm-flex justify-content-center justify-content-sm-between">
+		<span class="text-muted text-center text-sm-left d-block d-sm-inline-block"><a href= target="_blank">.</span>
+        <span class="text-muted float-none float-sm-right d-block mt-1 mt-sm-0 text-center pr-3">© 2025 Developted & Managed by ANANTA ICT Team</span>
+    </div>
+</footer>
